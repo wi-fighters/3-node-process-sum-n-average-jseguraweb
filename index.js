88608228b0706a701cc6d9a4cecd7be1cd1bb93d
Itamar Givon <itamar.givon.dev@gmail.com>
2019-10-29 16:56:30 +0100

    Add median

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ function average(numArray) {
   return sum(numArray) / numArray.length;
 }
 
+function median(numArray) {
+  const sorted = numArray.slice().sort();
+  const middle = (numArray.length - 1) / 2;
+  if (middle % 1 !== 0) {
+    return (sorted[Math.floor(middle)] + sorted[Math.ceil(middle)]) / 2;
+  }
+
+  return sorted[middle];
+}
+
 const args = process.argv.slice(2);
 const [operation, ...rest] = args;
 const numbers = rest.map(parseNumberArgs);
@@ -29,8 +39,11 @@ switch (operation) {
   case "avg":
     console.log(average(numbers));
     break;
+  case "med":
+    console.log(median(numbers));
+    break;
   default:
     console.log(
-      "I cannot calculate that, please type either Sum (sum) or Average (avg)"
+      'I cannot calculate that, please type either "sum" (to calculate the sum) or "avg" (To calculate the Average)';
     );
 }
